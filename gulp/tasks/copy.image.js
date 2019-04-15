c224c1c9f3d2f3422381feab8e04c9fe6f95d117
TorusHelm <TorusHelm@list.ru>
2019-04-15 19:20:31 +0500

    change path to styles in _template.pug, add browser-synd stream for images

--- gulp/tasks/copy.image.js
+++ gulp/tasks/copy.image.js
@@ -4,5 +4,6 @@ module.exports = function() {
   $.gulp.task('copy:image', function() {
     return $.gulp.src('./source/img/**/*.*', { since: $.gulp.lastRun('copy:image') })
       .pipe($.gulp.dest($.config.root + '/img'))
+      .pipe($.browserSync.stream());
   });
 };
